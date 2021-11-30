397028bd7c3c2b97e65288fd7495ce18a04d0771
miguelopesantana <84048244+miguelopesantana@users.noreply.github.com>
2021-11-30 11:52:29 +0000

    JavaDoc

---
+++ src/com/company/JavaDoc.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class JavaDoc {
+
+    public JavaDoc(){
+
+    }
+
+    public JavaDoc(int n){
+
+    }
+
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+        System.out.printf("Hello JavaDoc: %s.\n",args[0]);
+    }
+}
